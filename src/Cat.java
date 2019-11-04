c28bf2c6e16e9c9578bbb21a8634106d9da4dcf7
George <george.lashko@gmail.com>
2019-11-04 21:39:47 +0300

    Task01

--- src/Cat.java
+++ src/Cat.java
@@ -1,4 +1,23 @@
-package PACKAGE_NAME;
-
 public class Cat {
+    String name;
+
+    Cat(String name){
+        this.name = name;
+    }
+
+    void mew(){
+        System.out.println("meow");
+    }
+
+    void purr(){
+        System.out.println("mrrrr");
+    }
+
+    void getName(){
+        System.out.println(this.name);
+    }
+     SetName(name){
+
+     }
+
 }
