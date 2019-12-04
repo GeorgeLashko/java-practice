85172a49f2694f73e0fd757a3a7f91fecbee99ec
George <george.lashko@gmail.com>
2019-12-04 21:37:55 +0300

    Work from List of Elements

---
+++ src/com/rakovets/course/util/map/Element.java
@@ -0,0 +1,16 @@
+package com.rakovets.course.util.map;
+
+public class Element {
+    private int number;
+
+    public int getNumber() {
+        return number;
+    }
+
+    @Override
+    public String toString() {
+        return "Element{" +
+                "number=" + number +
+                '}';
+    }
+}
---
+++ src/com/rakovets/course/util/map/OnlineList.java
@@ -0,0 +1,10 @@
+package com.rakovets.course.util.map;
+
+public class OnlineList {
+    public static void main(String[] args) {
+        boolean isWorking = true;
+        while (isWorking){
+
+        }
+    }
+}
